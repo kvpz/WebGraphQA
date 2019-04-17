a51b6a08bbb2b548efdd53ef62c5a9b9ae06116c
kevin perez <unknotted26@gmail.com>
2019-04-17 11:02:00 -0400

    main/java/com.wg/TestDriver.java has now been moved to test/java/com.wgtest

---
+++ src/test/java/com/wgtest/WGTest.java
@@ -0,0 +1,63 @@
+package com.wgtest;
+
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WGTest {
+    public static List<String> getLinks(FirefoxDriver driver) {
+        String[] tags = {"a", "area", "base", "link"};
+        String[] attributes = {"href", "data-config-url", "ng-href"};
+        List<String> links = new ArrayList<String>();
+        for(int _i = 0; _i < 4; ++_i) {
+            List<WebElement> linkElements = driver.findElementsByTagName(tags[_i]);
+            for(WebElement webElement : linkElements) {
+                try {
+                    //System.out.println(webElement.getAttribute("outerHTML"));
+                    String refLink = webElement.getAttribute("pathname"); // get the href value as seen in html
+                    String aRefLink = webElement.getAttribute("href");
+                    System.out.println(refLink);
+                    System.out.println(aRefLink);
+                    if (refLink != null && !refLink.equals("#")) {
+                        if (!webElement.getAttribute("href").contains("storage.google.com")) // avoid appending the incorrect domain to a path
+                            links.add("https://store.google.com" + refLink);
+                    } else {
+                        links.add(webElement.getAttribute("href")); // store hard link value assigned to href
+                    }
+
+                }
+                catch(StaleElementReferenceException e){
+                    System.out.println("Exception in getLinks" + e);
+                }
+            }
+        }
+
+        return links;
+    }
+
+    public static void main(String[] s) {
+        FirefoxDriver fd = new FirefoxDriver();
+        fd.get("https://store.google.com/");
+        ArrayList<String> links = (ArrayList<String>)getLinks(fd);
+        for (String link : links) {
+            System.out.println(link);
+        }
+        fd.close();
+    }
+
+    /*
+        Click the buy button.
+
+        classes the buy button can have:
+        primary transaction button
+
+     */
+    boolean ClickBuyButton() {
+        boolean truthy = false;
+
+        return truthy;
+    }
+}
