4bd83e3a8d615848a4d7400c5ecd4f3ed6e9da5c
kevin perez <unknotted26@gmail.com>
2019-04-17 11:39:40 -0400

    proper 404 test implemented

--- src/main/java/com/wg/GoogleStoreTests.java
+++ src/main/java/com/wg/GoogleStoreTests.java
@@ -2,7 +2,7 @@ package com.wg;
 
 import java.util.HashMap;
 import java.util.Map;
-
+import java.net.*;
 /**
     Contains the tests to perform on google store webpages
  */
@@ -10,10 +10,28 @@ public class GoogleStoreTests {
 
     /**
          Returns true if webPage is a 404 (which is not desirable!)
+
+        Reference: https://www.guru99.com/find-broken-links-selenium-webdriver.html
      */
     public static boolean Is404(WebPage webPage) {
         boolean status = false;
-        status = webPage.GetPageSource().contains("404");
+        HttpURLConnection huc = null;
+
+        try {
+            huc = (HttpURLConnection) (new URL(webPage.GetUrl())).openConnection();
+
+            huc.setRequestMethod("HEAD");
+
+            huc.connect();
+
+            int respCode = huc.getResponseCode();
+
+            if(respCode >= 404)
+                status = true;
+        }
+        catch(Exception e) {
+            System.out.println(e);
+        }
         return status;
     }
 
